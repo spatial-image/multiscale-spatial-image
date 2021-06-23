ac85eb324d9062ea6969bf109769f0fdb63d2184
Matt McCormick <matt.mccormick@kitware.com>
2021-06-23 14:21:05 -0400

    Package configuration

---
+++ README.md
@@ -0,0 +1,3 @@
+# spatial-image-multiscale
+
+Generate a multiscale [spatial-image](https://github.com/spatial-image/spatial-image).
---
+++ spatial_image_multiscale.py
@@ -0,0 +1,8 @@
+"""spatial-image-multiscale
+
+Generate a multiscale spatial image."""
+
+__version__ = "0.0.1"
+
+import xarray as xr
+import numpy as np
