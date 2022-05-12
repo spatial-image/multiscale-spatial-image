ENH: Add DASK_GAUSSIAN scaling and fix ITK_GAUSSIAN
Changes:
- Adds `DASK_GAUSSIAN` scaling option which combines dask_image Gaussian
  blurring with ITK's ResampleImageFilter for downsampling
- Resolves issue in ITK_GAUSSIAN where kernel radius was improperly set
  from local block metadata,
  resulting severely different images from DASK_GAUSSIAN blurring
  results
- Adds tests and update baseline images

multiscale_spatial_image.py
@@ -135,6 +135,7 @@ class Methods(Enum):
     XARRAY_COARSEN = "xarray.DataArray.coarsen"
     ITK_BIN_SHRINK = "itk.bin_shrink_image_filter"
     ITK_GAUSSIAN = "itk.discrete_gaussian_image_filter"
+    DASK_GAUSSIAN = "dask_image.ndfilters.gaussian_filter"
 
 
 def to_multiscale(
@@ -221,6 +222,29 @@ def to_multiscale(
             current_input = current_input.chunk(aligned_chunks)
 
         return current_input
+
+    def get_block(xarray_image, block_index:int):
+        '''Helper method for accessing an enumerated chunk from xarray input'''
+        block_shape = [c[block_index] for c in current_input.chunks]
+        block = current_input[tuple([slice(0, s) for s in block_shape])]
+        # For consistency for now, do not utilize direction until there is standardized support for
+        # direction cosines / orientation in OME-NGFF
+        block.attrs.pop("direction", None)
+        return block   
+
+    def compute_sigma(input_spacings, shrink_factors) -> list:
+        '''
+        Compute Gaussian kernel sigma values for resampling to isotropic spacing.
+        Input and output lists are assumed to be in xyzt order
+        sigma = sqrt((isoSpacing^2 - inputSpacing[0]^2)/(2*sqrt(2*ln(2)))^2)
+        Ref https://discourse.itk.org/t/resampling-to-isotropic-signal-processing-theory/1403/16
+        '''
+        assert len(input_spacings) == len(shrink_factors)
+        import math
+        output_spacings = [input_spacing * shrink for input_spacing, shrink in zip(input_spacings, shrink_factors)]
+        denominator = (2 * ((2 * math.log(2)) ** 0.5)) ** 2
+        return [((output_spacing ** 2 - input_spacing ** 2) / denominator) ** 0.5
+                for input_spacing, output_spacing in zip(input_spacings, output_spacings)]
 
     if method is Methods.XARRAY_COARSEN:
         for factor_index, scale_factor in enumerate(scale_factors):
@@ -316,44 +340,20 @@ def to_multiscale(
             current_input = downscaled
     elif method is Methods.ITK_GAUSSIAN:
         import math
-        import itk
+        import itk         
 
-        def get_block(xarray_image, block_index:int):
-            '''Helper method for accessing an enumerated chunk from xarray input'''
-            block_shape = [c[block_index] for c in current_input.chunks]
-            block = current_input[tuple([slice(0, s) for s in block_shape])]
-            # For consistency for now, do not utilize direction until there is standardized support for
-            # direction cosines / orientation in OME-NGFF
-            block.attrs.pop("direction", None)
-            return block            
-
-        def compute_sigma(input_spacings, shrink_factors) -> list:
-            '''
-            Compute kernel sigma values for resampling to isotropic spacing.
-            Input and output lists are assumed to be in xyzt order
-            sigma = sqrt((isoSpacing^2 - inputSpacing[0]^2)/(2*sqrt(2*ln(2)))^2)
-            Ref https://discourse.itk.org/t/resampling-to-isotropic-signal-processing-theory/1403/16
-            '''
-            assert len(input_spacings) == len(shrink_factors)
-            output_spacings = [input_spacing * shrink for input_spacing, shrink in zip(input_spacings, shrink_factors)]
-            denominator = (2 * ((2 * math.log(2)) ** 0.5)) ** 2
-            return [((output_spacing ** 2 - input_spacing ** 2) / denominator) ** 0.5
-                    for input_spacing, output_spacing in zip(input_spacings, output_spacings)]
-
-        def compute_kernel_radius(xarray_block, shrink_factors) -> list:
+        def compute_kernel_radius(input_size, sigma_values, shrink_factors) -> list:
             '''Get kernel radius in xyzt directions'''
             DEFAULT_MAX_KERNEL_WIDTH = 32
             MAX_KERNEL_ERROR = 0.01
-            
-            image = itk.image_from_xarray(xarray_block)
-            image_dimension = image.GetImageDimension()
-            sigma_values = compute_sigma(itk.spacing(image), shrink_factors)
-            variance = [sigma ** 2 for sigma in sigma_values]
-            # Constrain kernel width to be at most the size of one chunk
-            max_kernel_width = min(DEFAULT_MAX_KERNEL_WIDTH, *itk.size(image))
+            image_dimension = len(input_size)
 
-            # Follow itk.DiscreteGaussianImageFilter procedure to generate directional kernels
+            # Constrain kernel width to be at most the size of one chunk
+            max_kernel_width = min(DEFAULT_MAX_KERNEL_WIDTH, *input_size)
+            variance = [sigma ** 2 for sigma in sigma_values]
+
             def generate_radius(direction:int) -> int:
+                '''Follow itk.DiscreteGaussianImageFilter procedure to generate directional kernels'''
                 oper = itk.GaussianOperator[itk.F, image_dimension]()
                 oper.SetDirection(direction)
                 oper.SetMaximumError(MAX_KERNEL_ERROR)
@@ -364,13 +364,17 @@ def to_multiscale(
 
             return [generate_radius(dim) for dim in range(image_dimension)]
 
-        def blur_and_downsample(xarray_data, shrink_factors, kernel_radius):
+        def blur_and_downsample(xarray_data, shrink_factors, sigma_values, kernel_radius):
             '''Blur and then downsample a given image chunk'''
-
-            # xarray chunk does not have metadata attached, input values are ITK defaults
+            
+            # xarray chunk does not have metadata attached, values are ITK defaults
             image = itk.image_view_from_array(xarray_data)
-            input_spacing = itk.spacing(image)
             input_origin = itk.origin(image)
+
+            # Skip this image block if it has 0 voxels
+            block_size = itk.size(image)
+            if(any([block_len == 0 for block_len in block_size])):
+                return None
             
             # Output values are relative to input
             itk_shrink_factors = shrink_factors  # xyzt
@@ -381,7 +385,6 @@ def to_multiscale(
                 for image_len, radius, shrink_factor in zip(itk.size(image), itk_kernel_radius, itk_shrink_factors)]
             
             # Construct pipeline
-            sigma_values = compute_sigma(input_spacing, shrink_factors)
 
             # Optionally run accelerated smoothing with itk-vkfft
             if 'VkFFTBackend' in dir(itk):
@@ -415,10 +418,12 @@ def to_multiscale(
             block_neg1_input = get_block(current_input,-1)
 
             # Compute overlap for Gaussian blurring for all blocks
-            kernel_radius = compute_kernel_radius(block_0_input, shrink_factors)
+            block_0_image = itk.image_from_xarray(block_0_input)
+            input_spacing = itk.spacing(block_0_image)
+            sigma_values = compute_sigma(input_spacing, shrink_factors)
+            kernel_radius = compute_kernel_radius(itk.size(block_0_image), sigma_values, shrink_factors)
 
             # Compute output size and spatial metadata for blocks 0, .., N-2
-            block_0_image = itk.image_from_xarray(block_0_input)
             filt = itk.BinShrinkImageFilter.New(
                 block_0_image, shrink_factors=shrink_factors
             )
@@ -463,6 +468,7 @@ def to_multiscale(
               blur_and_downsample,
               current_input.data,
               shrink_factors=shrink_factors,
+              sigma_values=sigma_values,
               kernel_radius=kernel_radius,
               dtype=dtype,
               depth={dim: radius for dim, radius in enumerate(np.flip(kernel_radius))}, # overlap is in tzyx
@@ -490,6 +496,127 @@ def to_multiscale(
                 name=image.name, promote_attrs=True
             )
             current_input = downscaled
+    elif method is Methods.DASK_GAUSSIAN:
+        import itk
+        import dask_image.ndfilters
+
+        def get_truncate(xarray_image, sigma_values, truncate_start=4.0) -> float:
+            '''Discover truncate parameter yielding a viable kernel width
+               for dask_image.ndfilters.gaussian_filter processing. Block overlap
+               cannot be greater than image size, so kernel radius is more limited
+               for small images. A lower stddev truncation ceiling for kernel
+               generation can result in a less precise kernel.'''
+
+            from dask_image.ndfilters._gaussian import _get_border
+
+            truncate = truncate_start
+            stddev_step = 0.5  # search by stepping down by 0.5 stddev in each iteration
+
+            border = _get_border(xarray_image.data, sigma_values, truncate)
+            while any([border_len > image_len for border_len, image_len in zip(border, xarray_image.shape)]):
+                truncate = truncate - stddev_step
+                if(truncate <= 0.0):
+                    break                    
+                border = _get_border(xarray_image.data, sigma_values, truncate)
+
+            return truncate
+
+
+        def downsample(xarray_data, shrink_factors):
+            '''Downsample a given image chunk'''
+
+            # xarray chunk does not have metadata attached, input values are ITK defaults
+            image = itk.image_view_from_array(xarray_data)
+            input_spacing = itk.spacing(image)
+            input_origin = itk.origin(image)
+            
+            # Skip this image block if it has 0 voxels
+            block_size = itk.size(image)
+
+            if(any([block_len == 0 for block_len in block_size])):
+                return None
+            
+            # Output values are relative to input
+            itk_shrink_factors = shrink_factors  # xyzt
+            output_spacing = [s * f for s, f in zip(itk.spacing(image), itk_shrink_factors)]
+            output_size = [max(0,int(image_len / shrink_factor))
+                for image_len, shrink_factor in zip(itk.size(image), itk_shrink_factors)]
+            
+            # Resample with ITK
+            return itk.resample_image_filter(image,
+                size=output_size,
+                output_spacing=output_spacing,
+                output_origin=input_origin)
+
+        for factor_index, scale_factor in enumerate(scale_factors):
+            dim_factors = dim_scale_factors(scale_factor)
+            current_input = align_chunks(current_input, dim_factors)
+
+            image_dims: Tuple[str, str, str, str] = ("x", "y", "z", "t")
+            shrink_factors = [dim_factors[sf] for sf in image_dims if sf in dim_factors]
+
+            # Compute metadata for region splitting
+
+            # Blocks 0, ..., N-2 have the same shape
+            block_0_input = get_block(current_input,0)
+            block_0_image = itk.image_from_xarray(block_0_input)
+
+            sigma_values = compute_sigma(itk.spacing(block_0_image), shrink_factors)
+
+            # Compute output size and spatial metadata for blocks 0, .., N-2
+            filt = itk.BinShrinkImageFilter.New(
+                block_0_image, shrink_factors=shrink_factors
+            )
+            filt.UpdateOutputInformation()
+            block_output = filt.GetOutput()
+            block_0_output_spacing = block_output.GetSpacing()
+            block_0_output_origin = block_output.GetOrigin()   # TODO examine underlying shift logic
+            
+            block_0_scale = {
+                image_dims[i]: s for (i, s) in enumerate(block_0_output_spacing)
+            }
+            block_0_translation = {
+                image_dims[i]: s for (i, s) in enumerate(block_0_output_origin)
+            }
+            dtype = block_output.dtype
+
+            # Discover viable truncate parameter
+            truncate = get_truncate(current_input, np.flip(sigma_values))
+
+            blurred_array = dask_image.ndfilters.gaussian_filter(
+                image=current_input.data,
+                sigma=np.flip(sigma_values), #tzyx order
+                mode='nearest',
+                truncate=truncate
+            )
+
+            downscaled_array = map_blocks(
+                downsample,
+                blurred_array,
+                shrink_factors=shrink_factors,
+                dtype=dtype).compute()
+            
+            downscaled = to_spatial_image(
+                downscaled_array,
+                dims=image.dims,
+                scale=block_0_scale,
+                translation=block_0_translation,
+                name=current_input.name,
+                axis_names={
+                    d: image.coords[d].attrs.get("long_name", d) for d in image.dims
+                },
+                axis_units={
+                    d: image.coords[d].attrs.get("units", "") for d in image.dims
+                },
+                t_coords=image.coords.get("t", None),
+                c_coords=image.coords.get("c", None),
+            )
+            downscaled = downscaled.chunk(out_chunks)
+            data_objects[f"scale{factor_index+1}"] = downscaled.to_dataset(
+                name=image.name, promote_attrs=True
+            )
+            current_input = downscaled
+
 
     multiscale = MultiscaleSpatialImage.from_dict(
         d=data_objects