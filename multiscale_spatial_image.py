ba7b1cfb2768296b3085f6ce34442fe90a9bd472
Matt McCormick <matt.mccormick@kitware.com>
2022-05-16 22:11:07 -0400

    ENH: Bump version to 0.7.0

--- multiscale_spatial_image.py
+++ multiscale_spatial_image.py
@@ -2,7 +2,7 @@
 
 Generate a multiscale spatial image."""
 
-__version__ = "0.6.0"
+__version__ = "0.7.0"
 
 import enum
 from typing import Union, Sequence, List, Optional, Dict, Mapping, Any, Tuple
