2e7ae81bb2fc7817219f44159abc5106f19351c0
Matt McCormick <matt.mccormick@kitware.com>
2022-09-20 16:11:42 -0400

    STYLE: Factor out test_to_multiscale_xarray.py

--- test/test_to_multiscale.py
+++ test/test_to_multiscale.py
@@ -3,7 +3,6 @@ import pytest
 from pathlib import Path
 
 import xarray as xr
-from zarr.storage import DirectoryStore
 from datatree import open_datatree
 
 from multiscale_spatial_image import Methods, to_multiscale, itk_image_to_multiscale
@@ -21,25 +20,6 @@ def test_base_scale(input_images):
     xr.testing.assert_equal(image, multiscale['scale0'].ds["small_head"])
     
 def test_isotropic_scale_factors(input_images):
-    
-    dataset_name = "cthead1"
-    image = input_images[dataset_name]
-    baseline_name = "2_4/XARRAY_COARSEN"
-    multiscale = to_multiscale(image, [2, 4], method=Methods.XARRAY_COARSEN)
-    verify_against_baseline(dataset_name, baseline_name, multiscale)
-
-    dataset_name = "cthead1"
-    image = input_images[dataset_name]
-    baseline_name = "2_3/XARRAY_COARSEN"
-    multiscale = to_multiscale(image, [2, 3], method=Methods.XARRAY_COARSEN)
-    verify_against_baseline(dataset_name, baseline_name, multiscale)
-
-    dataset_name = "small_head"
-    image = input_images[dataset_name]
-    baseline_name = "2_3_4/XARRAY_COARSEN"
-    multiscale = to_multiscale(image, [2, 3, 4], method=Methods.XARRAY_COARSEN)
-    verify_against_baseline(dataset_name, baseline_name, multiscale)
-
     dataset_name = "cthead1"
     image = input_images[dataset_name]
     multiscale = to_multiscale(image, [2, 4], method=Methods.ITK_BIN_SHRINK)
@@ -112,27 +92,6 @@ def test_label_gaussian_isotropic_scale_factors(input_images):
 
 
 def test_anisotropic_scale_factors(input_images):
-    dataset_name = "cthead1"
-    image = input_images[dataset_name]
-    scale_factors = [{"x": 2, "y": 4}, {"x": 1, "y": 2}]
-    multiscale = to_multiscale(image, scale_factors, method=Methods.XARRAY_COARSEN)
-    baseline_name = "x2y4_x1y2/XARRAY_COARSEN"
-    verify_against_baseline(dataset_name, baseline_name, multiscale)
-    # Test default method: Methods.XARRAY_COARSEN
-    multiscale = to_multiscale(image, scale_factors)
-    verify_against_baseline(dataset_name, baseline_name, multiscale)
-
-    dataset_name = "small_head"
-    image = input_images[dataset_name]
-    scale_factors = [
-        {"x": 3, "y": 2, "z": 4},
-        {"x": 2, "y": 2, "z": 2},
-        {"x": 1, "y": 2, "z": 1},
-    ]
-    multiscale = to_multiscale(image, scale_factors, method=Methods.XARRAY_COARSEN)
-    baseline_name = "x3y2z4_x2y2z2_x1y2z1/XARRAY_COARSEN"
-    verify_against_baseline(dataset_name, baseline_name, multiscale)
-
     dataset_name = "cthead1"
     image = input_images[dataset_name]
     scale_factors = [{"x": 2, "y": 4}, {"x": 1, "y": 2}]
