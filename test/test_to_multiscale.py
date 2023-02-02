9d6ea44382c7fb73d82777ba190778b937821dfa
giovp <giov.pll@gmail.com>
2023-02-02 10:40:10 +0100

    remove unused import

--- test/test_to_multiscale.py
+++ test/test_to_multiscale.py
@@ -1,19 +1,19 @@
 import xarray as xr
 import pytest
-import re
-from multiscale_spatial_image import Methods, to_multiscale 
+from multiscale_spatial_image import Methods, to_multiscale
 
 from ._data import input_images
+
 
 def test_base_scale(input_images):
     image = input_images["cthead1"]
 
     multiscale = to_multiscale(image, [])
-    xr.testing.assert_equal(image, multiscale['scale0'].ds["cthead1"])
+    xr.testing.assert_equal(image, multiscale["scale0"].ds["cthead1"])
 
     image = input_images["small_head"]
     multiscale = to_multiscale(image, [])
-    xr.testing.assert_equal(image, multiscale['scale0'].ds["small_head"])
+    xr.testing.assert_equal(image, multiscale["scale0"].ds["small_head"])
 
     with pytest.raises(ValueError):
         to_multiscale(image, scale_factors=[500])
