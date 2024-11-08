b43074a3288a1242ffde7bba8f7c1141890fd3f7
Wouter-Michiel Vierdag <michiel.vierdag@embl.de>
2024-11-08 17:56:50 +0100

    add decorator skipping nodes without dimensions

--- multiscale_spatial_image/__init__.py
+++ multiscale_spatial_image/__init__.py
@@ -7,9 +7,11 @@ __all__ = [
     "Methods",
     "to_multiscale",
     "itk_image_to_multiscale",
+    "skip_non_dimension_nodes",
     "__version__",
 ]
 
 from .__about__ import __version__
 from .multiscale_spatial_image import MultiscaleSpatialImage
 from .to_multiscale import Methods, to_multiscale, itk_image_to_multiscale
+from .utils import skip_non_dimension_nodes
